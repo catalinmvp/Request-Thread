cfbeb10387aed62fcfb64ec0ea070dacf33bc47c
catalinmvp <nichitaandrei21@yahoo.com>
2020-10-13 14:33:33 +0300

    Create Visa.java

---
+++ Visa.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hwthread;
+
+/**
+ *
+ * @author andrei.nichita0308
+ */
+public class Visa {
+    private String visa_candidateName;
+
+    public void setVisa_candidateName(String visa_candidateName) {
+        this.visa_candidateName = visa_candidateName;
+    }
+    
+    
+}
